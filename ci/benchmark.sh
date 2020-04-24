91cde8142de78e12e8abfa8ea8ade0b6406e82ae
Tim Wojtulewicz <tim@corelight.com>
2020-04-24 13:14:43 -0700

    Fixups
    - Only allow benchmarks from the main zeek repo
    - Use the CIRRUS_TASK_NAME when building urls
    - Pass the build file url directly instead of base64 encoded
    - Pass an md5 hash of the build file

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -2,12 +2,24 @@
 
 set -e
 
-CENTOS7_BUILD_URL="https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/centos8/binary/build.tgz"
-B64_BUILD_URL=$(echo -n ${CENTOS7_BUILD_URL} | base64 -i -)
+# Don't do this for any branch that isn't from the main zeek repo.
+# TODO: is it possible to do this from cirrus.yml instead of here?
+if [ "${CIRRUS_REPO_OWNER}" != "zeek" ]; then
+    exit 0
+fi
 
-# Generate an HMAC digest for the path plus a timestamp to send as an authentication header. openssl outputs a
-# hex string here so there's no need to base64 encode it.
+BUILD_URL="https://api.cirrus-ci.com/v1/artifact/build/${CIRRUS_BUILD_ID}/${CIRRUS_TASK_NAME}/binary/build.tgz"
+
+# Generate an md5 hash of the build file. We can do this here because the path to the
+# file still exists from the prior scripts.
+BUILD_HASH=$(md5sum build.tgz)
+
+# Generate an HMAC digest for the path plus a timestamp to send as an authentication
+# header. Openssl outputs a hex string here so there's no need to base64 encode it.
+# TODO: would it make sense to add the build hash as part of the hmac key here just
+# for more uniqueness?
 TIMESTAMP=$(date +'%s')
 HMAC_DIGEST=$(echo "/zeek${TIMESTAMP}" | openssl dgst -sha256 -hmac ${ZEEK_BENCHMARK_HMAC_KEY} | awk '{print $2}')
 
-#curl -X POST -H "Zeek-HMAC: ${HMAC_DIGEST}" -H "Zeek-HMAC-Timestamp: ${TIMESTAMP}" \"http://os-perf.colo.corelight.io:8000/zeek?branch=${CIRRUS_BRANCH}\&build=${B64_BUILD_URL}\"
+# Make a request to the benchmark host.
+echo curl -X POST -H "Zeek-HMAC: ${HMAC_DIGEST}" -H "Zeek-HMAC-Timestamp: ${TIMESTAMP}" \"http://os-perf:8080/zeek?branch=${CIRRUS_BRANCH}\&build=${BUILD_URL}\&build_hash=${BUILD_HASH}\"
